949d597a67d0f14e16c5e9eacd32c4fa608dd6e4
William Capozzoli <thinkocapo@gmail.com>
2023-08-20 23:24:56 +0200

    clean

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,37 @@
 use rand::Rng;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-
 use std::path::Path;
 
-
+// TODO warnings for derive(Debug) 
+// calling to_string()
+// modularizing file reader and line parsing. copy/ownership of who is using line reader, access to it.
 fn main() -> std::io::Result<()> {
-    // PRONOUNS
-    // Immutable and no methods needed yet. Maybe struct ones Word is evaluated for matching.
-    // pub struct Pronoun;
-    // let pronouns = ["Ich", "Du", "er/es/sie", "wir", "ihr", "sie/Sie"];
-    let pronouns = ["Ich", "Du", "Wir", "Ihr", "Sie"];
-    let pronoun_number = rand::thread_rng().gen_range(0..=pronouns.len()-1);
-    // let pronoun_pick = pronouns[pronoun_number];
-    let pronoun = pronouns[pronoun_number];
+
+    let pronouns = ["Ich", "Du", "er/es/sie", "Wir", "Ihr", "Sie"];
+    let random = rand::thread_rng().gen_range(0..=pronouns.len()-1);
+    let pronoun = pronouns[random];
     
     // NOUN
-    // singular, plural, gender. casing (nominative, accusative, dative) "noun Apfel Apfel m/f/n";
+    // casing (nominative, accusative, dative) and article()
     #[derive(Debug)]
     pub struct Noun{
         singular: String,
         plural: String,
-        gender: String // or takes def+indef articles (der, ein. das, ein. eine, die. and die for plural)
+        gender: String
     }
+
     impl Noun {
         fn quantity(&self) -> String {
-            let num = 1;
-            if num%2==0 {
+            let random = rand::thread_rng().gen_range(0..=1);
+            if random%2==0 {
                 return self.singular.to_string();
             } else {
                 return self.plural.to_string();
             }
         }
-        
-        //fn article(&self) -> String {
-            // if g == 'm' then "der" or "ein" [or none] + self.s.to_string();
-            // if g == 'n' then "das" or "ein" [or none]  + self.s.to_string();
-            // if g == 'f' then "die" or "eine" [or none]  + self.s.to_string();
-            // or the prompt could show, "Du verkaufen puppe 'singular' 'definite" singular/plural and def/indef as part of promt
-            // pick if want singular or plural?
-        //}
-        // assigns the articles needed for use later? but defer this part, if loading millions of words?
-        // ? assume pre-construction of der/das/der ein/ein/eine, only select 'definite' or 'indefinite'. and assumes Nominative case
-
-        // noun.express(DEFINITE) nah...
-        // noun.definite_article() nah...
-        // noun() yaa, and it picks for you :)
-        // fn new(&self) -> Self {
-            // casing Nominative (then definite, indefinite article) and a casing has articles in it?
-                // nominative.definite = "der" nominative.definite()
-                // nominative.indefinite = "der" nominative.definite()
-            // casing Accusative (then definite, indefinite article) and a casing has articles in it?
-                // accusative.definite = "der" nominative.definite()
-        //}
-        //fn to_string(&self) -> String {
-            // Plurality.pick() article = nominative.gender().plurality() ? .gender() returns 'M' or 'F' and then plurality uses that?
-            // 1 pick if want sing or plural (those are properties already) .plurality() .quantity()
-            // 2 pick Nominative case for now .case() .casing()
-            // 3 pick nominative.article() accusative.article() nominative.article(singular) or nominative.singular(article)
-            // or 
-            // der or ein...
-        //}
     }
 
-    // VERBS
     #[derive(Debug)]
     pub struct Verb{
         present: String,
@@ -81,18 +49,6 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    // struct Word<T> {
-    //     class : T
-    //     // value, data, next: Word
-    // }
-    // impl<T> Word<T> {
-        // pub fn new(class: T) -> Self {
-        //     Word { class }
-        // }
-        // pub fn next() -> Self {}
-    // }
-    
-    
     // NOUNS
     let nouns = Path::new("./src/nouns.txt");
     let file_nouns_1 = File::open(nouns)?;
@@ -102,7 +58,9 @@ fn main() -> std::io::Result<()> {
     let noun_count = file_nouns_reader_1.lines().count();
     let mut noun_line_number = 0;
     let random_noun_number = rand::thread_rng().gen_range(0..=noun_count-1);
+
     let mut noun = Noun{singular:"".to_string(), plural:"".to_string(),gender:"".to_string()};
+    
     for line in file_nouns_reader_2.lines() {
         let line = line?;
         let parts: Vec<&str> = line.trim().split(' ').collect();
@@ -138,9 +96,20 @@ fn main() -> std::io::Result<()> {
     }
 
     // noun() or even 'word()' could decide which to apply, and print...?
-    println!("\n pronoun {0} present: {1} past: {2} singular: {3} plural: {4} gender: {5}\n", pronoun, verb.present, verb.past, noun.singular, noun.plural, noun.gender);
-    println!("\n pronoun {0} verb: {1} noun: {2}\n", pronoun, verb.tense(), noun.quantity());
+    // println!("\n pronoun {0} present: {1} past: {2} singular: {3} plural: {4} gender: {5}\n", pronoun, verb.present, verb.past, noun.singular, noun.plural, noun.gender);
+    println!("\n {0} {1} {2}\n", pronoun, verb.tense(), noun.quantity());
 
+    // struct Word<T> {
+    //     class : T
+    //     // value, data, next: Word
+    // }
+    // impl<T> Word<T> {
+        // pub fn new(class: T) -> Self {
+        //     Word { class }
+        // }
+        // pub fn next() -> Self {}
+    // }
+    
     // let word = Word::new(Noun{s:"verkehr".to_string(), p:"verkehr".to_string(), g:"m".to_string()});
     // let word1 = Word::new(noun);
     // println!("word1 is: {:?}", word1.class);
