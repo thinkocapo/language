1b85e1bc3f63327822a1c948d1725038c89e0ea2
William Capozzoli <thinkocapo@gmail.com>
2023-08-22 23:28:09 +0200

    pick noun and verb functions

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::io::{BufReader, BufRead};
 use std::path::Path;
 
 // TODO
-// pick one of er/es/sie
-// modularizing file reader and line parsing. copy/ownership of who is using line reader, access to it.
+// copy/ownership of who is using line reader, access to it.
 
 // needs Akk/Dat if used as direct/indirect object, verb may need to specify that
     // sentence/word connects verb to noun, verb has akk/dat property or preference,
@@ -13,25 +12,28 @@ use std::path::Path;
     // or just print 'def' or 'indef'. could have also done print 'sing plural' and then you have to decide how noun written. Nah
 // Start presentation, or explaining
 
+// TODO modularize er es sie picker
 // TODO warnings for derive(Debug) 
 // calling to_string()
 // how to make noun() word() itself execute as a function without calling noun.method() - Constructor method? but on an instance?
 fn main() -> std::io::Result<()> {
 
-    // let pronouns = ["Ich", "Du", "Er Es Sie", "Wir", "Ihr", "Sie"];
-    let pronouns = ["Er Es Sie"];
-
+    let pronouns = ["Ich", "Du", "Er Es Sie", "Wir", "Ihr", "Sie"];
     let random = rand::thread_rng().gen_range(0..=pronouns.len()-1);
-
     let mut pronoun = pronouns[random];
     if pronoun == "Er Es Sie" {
         let random_pronoun = rand::thread_rng().gen_range(0..=2);
         let er_sie_es: Vec<&str> = pronoun.split(' ').collect();
         pronoun = er_sie_es[random_pronoun];
     }
+
+    let noun: Noun = pick_noun("./src/nouns.txt").unwrap();
+    let verb: Verb = pick_verb("./src/verbs.txt").unwrap();
     
-    // NOUN
-    // casing (nominative, accusative, dative) and article()
+    // println!("\n pronoun {0} present: {1} past: {2} singular: {3} plural: {4} gender: {5}\n", pronoun, verb.present, verb.past, noun.singular, noun.plural, noun.gender);
+    println!("\n {0} {1} {2}\n", pronoun, verb.tense(), noun.quantity());
+
+    
     #[derive(Debug)]
     pub struct Noun{
         singular: String,
@@ -67,76 +69,58 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    // NOUNS
-    let nouns = Path::new("./src/nouns.txt");
-    let file_nouns_1 = File::open(nouns)?;
-    let file_nouns_2 = File::open(nouns)?;
-    let file_nouns_reader_1 = BufReader::new(file_nouns_1);
-    let file_nouns_reader_2 = BufReader::new(file_nouns_2);
-    let noun_count = file_nouns_reader_1.lines().count();
-    let mut noun_line_number = 0;
-    let random_noun_number = rand::thread_rng().gen_range(0..=noun_count-1);
+    fn pick_noun(file_path: &str) -> Result<Noun, Box<dyn std::error::Error>> {
 
-    let mut noun = Noun{singular:"".to_string(), plural:"".to_string(),gender:"".to_string()};
+        let mut noun = Noun{singular:"".to_string(), plural:"".to_string(),gender:"".to_string()};
+
+        let nouns = Path::new(file_path);
+        let file_nouns_1 = File::open(nouns)?;
+        let file_nouns_reader_1 = BufReader::new(file_nouns_1);
+        let noun_count = file_nouns_reader_1.lines().count();
+        let random_noun_number = rand::thread_rng().gen_range(0..=noun_count-1);
     
-    for line in file_nouns_reader_2.lines() {
-        let line = line?;
-        let parts: Vec<&str> = line.trim().split(' ').collect();
-        // for word in line.trim().split(' ') {
-            // println!("{}", word);
-        // }
-        if random_noun_number == noun_line_number {
-            noun = Noun{singular:parts[0].to_string(), plural: parts[1].to_string(), gender:String::from(parts[2])};
+        let file_nouns_2 = File::open(nouns)?;
+        let file_nouns_reader_2 = BufReader::new(file_nouns_2);
+    
+        // Pick a random Noun upon iterating through the lines from the file. this would be easier if I could convert 'lines' into an Array and access by index Id
+        let mut noun_line_number = 0;
+        for line in file_nouns_reader_2.lines() {
+            let line = line?;
+            let parts: Vec<&str> = line.trim().split(' ').collect();
+    
+            if random_noun_number == noun_line_number {
+                noun = Noun{singular:parts[0].to_string(), plural: parts[1].to_string(), gender:String::from(parts[2])};
+            }
+            noun_line_number = noun_line_number+1;
         }
-        noun_line_number = noun_line_number+1;
+
+        Ok(noun)
     }
 
-    // VERBS
-    let verbs = Path::new("./src/verbs.txt");
-    
-    let file = File::open(verbs)?;
-    let file2 = File::open(verbs)?;
-    let reader = BufReader::new(file);
-    let reader2 = BufReader::new(file2);
+    fn pick_verb(file_path: &str) -> Result<Verb, Box<dyn std::error::Error>> {
+        let verbs = Path::new(file_path);
+        let file = File::open(verbs)?;
+        let file2 = File::open(verbs)?;
+        let reader = BufReader::new(file);
+        let reader2 = BufReader::new(file2);
 
-    let verb_count = reader2.lines().count();
-    let mut verb_line_number = 0;
-    let random_verb_number = rand::thread_rng().gen_range(0..=verb_count-1);
-    
-    let mut verb = Verb{present:"".to_string(),past:"".to_string()};
-
-    for line in reader.lines() {
-        let line = line?;
-        let parts: Vec<&str> = line.trim().split(' ').collect();
+        let verb_count = reader2.lines().count();
+        let mut verb_line_number = 0;
+        let random_verb_number = rand::thread_rng().gen_range(0..=verb_count-1);
         
-        if random_verb_number == verb_line_number {
-            verb = Verb{present: parts[0].to_string(), past: parts[1].to_string()};
-        }   
-        verb_line_number = verb_line_number+1;
+        let mut verb = Verb{present:"".to_string(),past:"".to_string()};
+
+        for line in reader.lines() {
+            let line = line?;
+            let parts: Vec<&str> = line.trim().split(' ').collect();
+            
+            if random_verb_number == verb_line_number {
+                verb = Verb{present: parts[0].to_string(), past: parts[1].to_string()};
+            }   
+            verb_line_number = verb_line_number+1;
+        }
+        Ok(verb)
     }
-
-    // noun() or even 'word()' could decide which to apply, and print...?
-    // println!("\n pronoun {0} present: {1} past: {2} singular: {3} plural: {4} gender: {5}\n", pronoun, verb.present, verb.past, noun.singular, noun.plural, noun.gender);
-    println!("\n {0} {1} {2}\n", pronoun, verb.tense(), noun.quantity());
-
-    // struct Word<T> {
-    //     class : T
-    //     // value, data, next: Word
-    // }
-    // impl<T> Word<T> {
-        // pub fn new(class: T) -> Self {
-        //     Word { class }
-        // }
-        // pub fn next() -> Self {}
-    // }
-    
-    // let word = Word::new(Noun{s:"verkehr".to_string(), p:"verkehr".to_string(), g:"m".to_string()});
-    // let word1 = Word::new(noun);
-    // println!("word1 is: {:?}", word1.class);
-    // let word2 = Word::new(pronoun);
-    // println!("word2 is: {:?}", word2.class);
-    // let word3 = Word::new(verb);
-    // println!("word3 is: {:?}", word3.class);
 
     Ok(())
 }
